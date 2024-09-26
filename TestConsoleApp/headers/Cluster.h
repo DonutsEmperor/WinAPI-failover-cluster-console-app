c6247e201eb9b15948b6f2ee2dfc1a0c97b2ef47
NikitaDonkey <polovykhnikita@gmail.com>
2024-09-27 02:30:21 +0700

    Learning of including and linkage

---
+++ TestConsoleApp/headers/Cluster.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <list>
+
+class Cluster;
+typedef Cluster* PCluster;
+
+class Resource; class Node; class Group;
+class SharedVolume; class Network; class NetInterface; class ResourceType;
+
+#include "ClusterObjectProps.h"
+#include "ClusterObject.h"
+
+class Cluster {
+public:
+    const HCLUSTER mPCluster;
+    const std::wstring mCName;
+
+    //Containers for cluster items
+    std::list<Node> mNodes;
+    std::list<Group> mGroups;
+    std::list<Resource> mResources;
+    std::list<ResourceType> mResTypes;
+    std::list<Network> mNetworks;
+    std::list<NetInterface> mNetInterfaces;
+    std::list<SharedVolume> mCSVs;
+
+    Cluster(const HCLUSTER pCluster, const std::wstring& clusterName)
+        : mPCluster(pCluster), mCName(clusterName) {
+        FetchEnumerations(CLUSTER_ENUM_NODE);
+        FetchEnumerations(CLUSTER_ENUM_GROUP);
+        FetchEnumerations(CLUSTER_ENUM_RESOURCE);
+        FetchEnumerations(CLUSTER_ENUM_RESTYPE);
+        FetchEnumerations(CLUSTER_ENUM_NETWORK);
+        FetchEnumerations(CLUSTER_ENUM_NETINTERFACE);
+        FetchEnumerations(CLUSTER_ENUM_SHARED_VOLUME_RESOURCE);
+    }
+
+    ~Cluster() {
+        CloseCluster(mPCluster);
+    }
+
+private:
+    void FetchEnumerations(const DWORD typeofEnum);
+    void HandleNode(PCLUSTER_ENUM_ITEM pItem);
+    void HandleGroup(PCLUSTER_ENUM_ITEM pItem);
+    void HandleResource(PCLUSTER_ENUM_ITEM pItem);
+    void HandleResType(PCLUSTER_ENUM_ITEM pItem);
+    void HandleNetwork(PCLUSTER_ENUM_ITEM pItem);
+    void HandleNetInterface(PCLUSTER_ENUM_ITEM pItem);
+    void HandleSharedVolume(PCLUSTER_ENUM_ITEM pItem);
+};
+
+#include "Node.h"
+#include "Resource.h"
+#include "Group.h"
+
+class ResourceType : public ClusterObject {
+public:
+    ResourceType(PCluster pCluster, PCLUSTER_ENUM_ITEM pWinStruct)
+        : ClusterObject(pCluster, pWinStruct) {}
+};
+
+class Network : public ClusterObject {
+public:
+    Network(PCluster pCluster, PCLUSTER_ENUM_ITEM pWinStruct)
+        : ClusterObject(pCluster, pWinStruct) {}
+};
+
+class NetInterface : public ClusterObject {
+public:
+    NetInterface(PCluster pCluster, PCLUSTER_ENUM_ITEM pWinStruct)
+        : ClusterObject(pCluster, pWinStruct) {}
+};
+
+class SharedVolume : public ClusterObject {
+public:
+    SharedVolume(PCluster pCluster, PCLUSTER_ENUM_ITEM pWinStruct)
+        : ClusterObject(pCluster, pWinStruct) {}
+};
---
+++ TestConsoleApp/headers/Group.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class Group : public ClusterObject {
+    HGROUP mPGroup;
+public:
+    std::wstring csvName;
+
+    Group(const PCluster pCluster, const PCLUSTER_ENUM_ITEM pWinStruct) 
+        : ClusterObject(pCluster, pWinStruct) {
+        mPGroup = OpenClusterGroup(pCluster->mPCluster, pWinStruct->lpszName);
+    }
+    ~Group() {
+        CloseClusterGroup(mPGroup);
+    }
+};
